a3090597b51e9ad98373bc0d9ce843d1ff589c61
zchexmix <zachchecketts9@gmail.com>
2024-01-11 14:01:35 +0000

    Date after copyright via js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,6 @@
 let modified = document.lastModified;
 document.getElementById("lastmodified").innerHTML = `Last modified: ${modified}`;
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("footer").innerHTML = `&copy; ${year} | Zach Checketts | London, England`;
