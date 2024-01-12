beb202bbb1688dcc585a6bdd4d270ac2b4e0f457
zchexmix <zachchecketts9@gmail.com>
2024-01-12 11:44:57 +0000

    Dark Mode, Hamburger button, css normalize

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,24 @@ document.getElementById("lastmodified").innerHTML = `Last modified: ${modified}`
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("footer").innerHTML = `&copy; ${year} | Zach Checketts | London, England`;
+
+const nav = document.querySelector('.navbar');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    nav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+const header = document.querySelector('.header');
+const darkBtn = document.querySelector('#darkmode');
+const main = document.querySelector('main');
+const body = document.querySelector('body');
+
+darkBtn.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    header.classList.toggle('dark');
+    main.classList.toggle('dark');
+    darkBtn.classList.toggle('dark');
+    hambutton.classList.toggle('dark');
+});
