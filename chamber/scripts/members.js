7e9bcc103fe8161495acd341e94406b808e8c39b
Zach Checketts <Zachchecketts9@gmail.com>
2024-02-02 23:21:05 +0000

    Drectory page styling and js

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,14 +1,15 @@
-membersSection = document.querySelector('#members');
+articleElement = document.querySelector('.members');
 url = "https://zchexmix.github.io/wdd230/chamber/data/members.json";
 
 async function getMembers() {
     try {
     
-        repsonse = await fetch(url);
+        const response = await fetch(url);
 
-        if (Response.ok) {
-            data = await response.json();
+        if (response.ok) {
+            const data = await response.json();
             console.log(data);
+            displayMembers(data.members);
         } else {
 
             throw new Error(`Error: ${response.statusText}`);
@@ -19,3 +20,42 @@ async function getMembers() {
     }
 
 }
+getMembers();
+
+const displayMembers = (members)=> {
+    members.forEach(member => {
+        const sect = document.createElement('section');
+        const imgElement = document.createElement('img');
+        const bizName = document.createElement('h4');
+        const bizAddress = document.createElement('p');
+        const bizNum = document.createElement('p');
+        const bizMembership = document.createElement('p');
+        const bizSite = document.createElement('a');
+
+        bizName.setAttribute('id', 'biz-name');
+        bizAddress.setAttribute('id', 'address');
+        bizNum.setAttribute('id', 'number');
+        bizSite.setAttribute('id', 'biz-site');
+
+        imgElement.setAttribute('src', member.img);
+        imgElement.setAttribute('alt', 'Company Logo');
+        imgElement.setAttribute('width', '240');
+        imgElement.setAttribute('height', 'auto');
+        bizName.textContent = member.name;
+        bizAddress.textContent = member.address;
+        bizNum.textContent = member.number;
+        bizSite.textContent = `${member.name} Website`;
+        bizSite.setAttribute('href', member.url);
+        bizMembership.textContent = member.membership;
+
+        articleElement.appendChild(sect);
+        sect.appendChild(imgElement);
+        sect.appendChild(bizName);
+        sect.appendChild(bizAddress);
+        sect.appendChild(bizNum);
+        sect.appendChild(bizSite);
+        sect.appendChild(bizMembership);
+
+    });
+}
+
