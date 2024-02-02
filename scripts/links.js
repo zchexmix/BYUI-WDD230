77971d4d3f7bbd22c0395176c5264cd0e34ba98e
Zach Checketts <Zachchecketts9@gmail.com>
2024-02-02 19:45:18 +0000

    home page week5 assignment

--- scripts/links.js
+++ scripts/links.js
@@ -1,6 +1,6 @@
 const gitUrl = "https://zchexmix.github.io/wdd230/";
-const linkUrl = "https://zchexmix.github.io/wdd230/scripts/links.js";
-
+const linkUrl = "https://zchexmix.github.io/wdd230/data/links.json";
+const ul = document.querySelector("#assignment-list");
 async function getLinks() {
     try {
 
@@ -9,6 +9,7 @@ async function getLinks() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+            displayWeeks(data.weeks);
         } else {
 
             throw new Error(`Error: ${response.statusText}`);
@@ -22,3 +23,30 @@ async function getLinks() {
     
 }
 getLinks();
+
+
+const displayWeeks = (weeks) => {
+    weeks.forEach((week) => {
+        const li = document.createElement("li");
+        
+        li.textContent = `${week.week}: `;
+        week.links.forEach ((link, index) => {
+            const aTag = document.createElement("a");
+            aTag.setAttribute('href', `${link.url}`);
+            aTag.textContent = `${link.title}  `;
+            const span = document.createElement('span');
+            span.textContent =  ' | ';
+            if (index === week.links.length - 1) {
+                span.textContent = " "
+            }
+            li.appendChild(aTag);
+            li.appendChild(span)
+            
+        });
+        ul.appendChild(li);
+        
+        
+    });
+    
+
+}        
